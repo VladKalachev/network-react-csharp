17ac2d14282e03bf4c06e4a4ad50f4a0a55c52d8
Калачев Владислав Алексеевич <vkalachev@technoserv.com>
2019-12-24 14:59:21 +0300

    add layout client-app

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { Header, Icon, List } from 'semantic-ui-react';
+import React, { useState, useEffect, Fragment } from 'react';
+import { Header, Icon, List, Container } from 'semantic-ui-react';
 import axios from 'axios';
+import { IActivities } from '../../models/Activities';
+import NavBar from '../../features/nav/NavBar';
 
 const App = () => {
 
-  const [values, setValues] = useState([])
+  const [activities, setActivities] = useState<IActivities[]>([])
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/values').then(response => {
-      setValues(response.data);
+    axios.get<IActivities[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
     });
-  }, [setValues])
+  }, [])
 
   return (
-    <div>
-      <Header as='h2'>
-        <Icon name='users' />
-        <Header.Content>Reactivities</Header.Content>
-      </Header>
-      <List>
-        {values.map((value: any) => (
-          <List.Item key={value.id}>{value.name}</List.Item>
-        ))}
-      </List>
-  </div>
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+         <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container> 
+  </Fragment>
   );
 }
 
---
+++ Reactivities/client-app/src/models/Activities.tsx
@@ -0,0 +1,9 @@
+export interface IActivities {
+    id: string;
+    title: string; 
+    description: string; 
+    category: string;
+    date: Date;
+    city: string;
+    venue: string;
+};
