0effbc0197cd1a264f0e985b3b597e5bcac72be9
Corvax <vlvete1990@yandex.ru>
2020-01-18 22:45:28 +0300

    adding Profile configuration

--- Reactivities/Application/User/Login.cs
+++ Reactivities/Application/User/Login.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -60,7 +61,7 @@ namespace Application.User
                         DisplayName = user.DisplayName,
                         Token = _jwtGenerator.CreateToken(user),
                         Username = user.UserName,
-                        Image = null
+                        Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
                     };
                 }
 
