76f06c16a8f90b7b65fede67c62ae47e9ccafaa8
Corvax <vlvete1990@yandex.ru>
2020-01-19 20:52:07 +0300

    adding profiles and agent Profiles

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,61 +1,66 @@
 import React, { Fragment, useContext, useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from '../../features/nav/NavBar';
-import ActivityDashboad from '../../features/activities/dashboard/ActivityDashboad';
-import { observer } from "mobx-react-lite";
-import { Route, withRouter, RouteComponentProps, Switch } from "react-router-dom";
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboad';
+import { observer } from 'mobx-react-lite';
+import {
+  Route,
+  withRouter,
+  RouteComponentProps,
+  Switch
+} from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import NotFound from './NotFound';
 import {ToastContainer} from 'react-toastify';
-import LoginForm from '../../features/user/LoginForm';
 import { RootStoreContext } from '../stores/rootStore';
 import LoadingComponent from './LoadingComponent';
 import ModalContainer from '../common/modals/ModalContainer';
+import ProfilePage from '../../features/profiles/ProfilePage';
 
-const App: React.FC<RouteComponentProps> = ({location}) => {
+const App: React.FC<RouteComponentProps> = ({ location }) => {
   const rootStore = useContext(RootStoreContext);
   const {setAppLoaded, token, appLoaded} = rootStore.commonStore;
   const {getUser} = rootStore.userStore;
-  
+
   useEffect(() => {
-    if(token) {
-      getUser().finally(() => setAppLoaded());
+    if (token) {
+      getUser().finally(() => setAppLoaded())
     } else {
       setAppLoaded();
     }
-  }, [token, getUser, setAppLoaded]);
+  }, [getUser, setAppLoaded, token])
 
-  if(!appLoaded) return <LoadingComponent content='Loading app...'/>
+  if (!appLoaded)  return <LoadingComponent content='Loading app...' />
 
   return (
     <Fragment>
       <ModalContainer />
       <ToastContainer position='bottom-right' />
       <Route exact path='/' component={HomePage} />
-      <Route path={`/(.+)`}
+      <Route
+        path={'/(.+)'}
         render={() => (
           <Fragment>
-            <NavBar/>
+            <NavBar />
             <Container style={{ marginTop: '7em' }}>
               <Switch>
-                <Route exact path='/activities' component={ActivityDashboad} />
+                <Route exact path='/activities' component={ActivityDashboard} />
                 <Route path='/activities/:id' component={ActivityDetails} />
-                <Route 
-                  key={location.key} 
-                  path={['/createActivity', '/manage/:id']} 
+                <Route
+                  key={location.key}
+                  path={['/createActivity', '/manage/:id']}
                   component={ActivityForm}
                 />
-                <Route path='/login' component={LoginForm} />
+                <Route path='/profile/:username' component={ProfilePage} />
                 <Route component={NotFound} />
               </Switch>
-             
             </Container>
           </Fragment>
         )}
-      /> 
-  </Fragment>
+      />
+    </Fragment>
   );
 };
 
