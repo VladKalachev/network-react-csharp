8866024d9554a6a3b79ec0f4fe01015575aa3f30
VladKalachev <vlvete1990@yandex.ru>
2020-01-22 11:48:16 +0300

    configuring Auth for SignalR

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Threading.Tasks;
 using API.Middleware;
 using API.SignalR;
 using Application.Activities;
@@ -108,6 +109,20 @@ namespace API
                         IssuerSigningKey = key,
                         ValidateAudience = false,
                         ValidateIssuer = false
+                    };
+                    opt.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context => 
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
+                            {
+                                context.Token = accessToken;
+                            }
+
+                            return Task.CompletedTask;
+                        }
                     };
                 });
             
