f23b292a4f7c8c3a3a162056adc33cd129e29ffe
VladKalachev <vlvete1990@yandex.ru>
2020-01-23 11:58:52 +0300

    Adding UserFollower Entity

--- Reactivities/Persistence/DataContext.cs
+++ Reactivities/Persistence/DataContext.cs
@@ -16,6 +16,7 @@ namespace Persistence
         public DbSet<UserActivity> UserActivities { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<UserFollowing> Followings { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -36,10 +37,25 @@ namespace Persistence
                 .WithMany(a => a.UserActivities)
                 .HasForeignKey(u => u.AppUserId);
 
-             builder.Entity<UserActivity>()
-                .HasOne(a => a.Activity)
-                .WithMany(u => u.UserActivities)
-                .HasForeignKey(a => a.ActivityId);    
+            builder.Entity<UserActivity>()
+            .HasOne(a => a.Activity)
+            .WithMany(u => u.UserActivities)
+            .HasForeignKey(a => a.ActivityId);
+
+            builder.Entity<UserFollowing>(b =>
+            {
+                b.HasKey(k => new { k.ObserverId, k.TargetId });
+
+                b.HasOne(o => o.Observer)
+                    .WithMany(f => f.Followings)
+                    .HasForeignKey(o => o.ObserverId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                b.HasOne(o => o.Target)
+                    .WithMany(f => f.Followers)
+                    .HasForeignKey(o => o.TargetId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });  
         }
     }
 }
